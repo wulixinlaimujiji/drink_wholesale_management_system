5f951284f700b8af894e1290abc7b676d5822d80
Xxx <x1050692380@gmail.com>
2024-03-02 22:32:02 +0800

    readme中添加系统概述

---
+++ drink_wholesale_management_system/cmake-build-debug/CMakeFiles/drink_wholesale_management_system.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drink_wholesale_management_system.dir/main.c.obj"
+  "CMakeFiles/drink_wholesale_management_system.dir/main.c.obj.d"
+  "CMakeFiles/drink_wholesale_management_system.dir/manager.c.obj"
+  "CMakeFiles/drink_wholesale_management_system.dir/manager.c.obj.d"
+  "CMakeFiles/drink_wholesale_management_system.dir/public.c.obj"
+  "CMakeFiles/drink_wholesale_management_system.dir/public.c.obj.d"
+  "CMakeFiles/drink_wholesale_management_system.dir/user.c.obj"
+  "CMakeFiles/drink_wholesale_management_system.dir/user.c.obj.d"
+  "drink_wholesale_management_system.exe"
+  "drink_wholesale_management_system.exe.manifest"
+  "drink_wholesale_management_system.pdb"
+  "libdrink_wholesale_management_system.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/drink_wholesale_management_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ drink_wholesale_management_system/cmake-build-debug/CMakeFiles/drink_wholesale_management_system.dir/main.c.obj.d
@@ -0,0 +1,34 @@
+CMakeFiles/drink_wholesale_management_system.dir/main.c.obj: \
+ E:\MyData\Document\Project\Course_Design\c\drink_wholesale_management_system\main.c \
+ E:\MyData\Document\Project\Course_Design\c\drink_wholesale_management_system\public.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/stdio.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/swprintf.inl \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/stdlib.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/corecrt_wstdlib.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/limits.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/syslimits.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/limits.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/crtdefs.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdlib_s.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/malloc.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/mm_malloc.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/errno.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stdbool.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/string.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/sec_api/string_s.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/time.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/sys/timeb.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/sec_api/sys/timeb_s.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/_timeval.h \
+ D:/IDE/Jetbrains/CLion/CLion\ 2022.3/bin/mingw/x86_64-w64-mingw32/include/pthread_time.h \
+ E:\MyData\Document\Project\Course_Design\c\drink_wholesale_management_system\manager.h \
+ E:\MyData\Document\Project\Course_Design\c\drink_wholesale_management_system\user.h
