f27bde92fff8961509d6f9233740f71d15c2e8f9
Xxx <x1050692380@gmail.com>
2024-03-03 15:00:46 +0800

    调整文件结构

--- drink_wholesale_management_system/cmake-build-debug/cmake_install.cmake
+++ drink_wholesale_management_system/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/MyData/Document/Project/Course_Design/c/drink_wholesale_management_system
+# Install script for directory: E:/MyData/Document/Project/Course_Design/C/drink_wholesale_management_system
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/MyData/Document/Project/Course_Design/c/drink_wholesale_management_system/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/MyData/Document/Project/Course_Design/C/drink_wholesale_management_system/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
